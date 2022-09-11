393212a12b6625a94b14e69d2b987085db627066
Kevin Tran <kevtran@hotmail.com>
2022-09-11 14:12:05 -0700

    upload page completed

--- kevin-tran-brainflix/src/components/UploadVideo/UploadVideo.js
+++ kevin-tran-brainflix/src/components/UploadVideo/UploadVideo.js
@@ -12,46 +12,60 @@ export default class UploadVideo extends React.Component {
         return (
             <>
                 <main className="uploadvideo">
-                    <h1 className="uploadvideo__header">Upload Video</h1>
-                    <form className="uploadvideo__form">
-                        <div>
-                            <VideoPreview videoPreview={videoPreview} />
-                        </div>
-
-                        <div>
-                            <h3 className="uploadvideo__label">
-                                TITLE YOUR VIDEO
-                            </h3>
-                            <input
-                                className="uploadvideo__title-input"
-                                type="text"
-                                placeholder="Add a title to your video"
-                            />
-                            <h3 className="uploadvideo__label">
-                                ADD A VIDEO DESCRIPTION
-                            </h3>
-                            <textarea
-                                className="uploadvideo__description-input"
-                                placeholder="Add a description to your video"
-                                name=""
-                                minLength="4"
-                            ></textarea>
-                        </div>
-                        <span>
+                    <div className="uploadvideo__header">
+                        <h1 className="uploadvideo__header-text">
+                            Upload Video
+                        </h1>
+                        <span className="uploadvideo__line-container">
                             <Divider />
                         </span>
-                        <div>
+                    </div>
+                    <form className="uploadvideo__form">
+                        <div className="uploadvideo__form-container">
+                            <div>
+                                <VideoPreview videoPreview={videoPreview} />
+                            </div>
+
+                            <div className="uploadvideo__input-container">
+                                <div>
+                                    <h3 className="uploadvideo__label">
+                                        TITLE YOUR VIDEO
+                                    </h3>
+                                    <input
+                                        className="uploadvideo__title-input"
+                                        type="text"
+                                        placeholder="Add a title to your video"
+                                    />
+                                </div>
+                                <div class="uploadvideo__description-container">
+                                    <h3 className="uploadvideo__label">
+                                        ADD A VIDEO DESCRIPTION
+                                    </h3>
+                                    <textarea
+                                        className="uploadvideo__description-input"
+                                        placeholder="Add a description to your video"
+                                        name=""
+                                        minLength="4"
+                                    ></textarea>
+                                </div>
+                            </div>
+                        </div>
+                        <span className="uploadvideo__line-container">
+                            <Divider />
+                        </span>
+                        <div className="uploadvideo__upload-container">
                             <Button
                                 buttonIcon={publishIcon}
                                 buttonText={"PUBLISH"}
                             />
-                            <Link to="/" className="uploadvideo__cancelbox">
-                                <h3 className="uploadvideo__cancel">Cancel</h3>
+                            <Link to="/" className="uploadvideo__cancel">
+                                <h3 className="uploadvideo__cancel-text">
+                                    CANCEL
+                                </h3>
                             </Link>
                         </div>
                     </form>
                 </main>
-                ;
             </>
         );
     }
