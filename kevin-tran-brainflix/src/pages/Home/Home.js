dc5ad7b70503f0f1c994d73600542df1b3884b48
Kevin Tran <kevtran@hotmail.com>
2022-09-11 19:30:50 -0700

    Brainflix Sprint 2 Submission v1.0

--- kevin-tran-brainflix/src/pages/Home/Home.js
+++ kevin-tran-brainflix/src/pages/Home/Home.js
@@ -6,7 +6,7 @@ import NextVideo from "../../components/NextVideo/NextVideo";
 import "./Home.scss";
 import axios from "axios";
 
-class Home extends React.Component {
+export default class Home extends React.Component {
     constructor() {
         super();
         this.apiKey = "81032e04-2c58-40b5-99e9-45fc20349a23";
@@ -17,14 +17,16 @@ class Home extends React.Component {
         this.mainHeroId = "";
         this.mainHeroDataDetails = "";
 
-        // for the video sidebar
+        // for the next video sidebar
         this.nextVideoData = "";
 
+        // initial state
         this.state = {
             currentHero: [],
             dataSummary: [],
         };
     }
+
     // GET video data for mounting
     getVideos = () =>
         axios
@@ -34,33 +36,49 @@ class Home extends React.Component {
                     this.apiKey
             )
             .then((response) => {
+                // Store video summary data into object variable
                 this.videoData = response.data;
-                // invoke to get video detail data
-                const videoParams = this.props.match.params.videoId;
-                if (!videoParams) {
-                    this.getVideoDetail(this.videoData[0].id);
+
+                // Store video params ID into variable
+                const videoIdParams = this.props.match.params.videoId;
+
+                // If Video ID is undefined (usually on initial load), Load the first video as default
+                if (!videoIdParams) {
+                    // Remove the first video from the next video sidebar
                     this.nextVideoData = response.data.filter((var1) => {
                         return var1.id !== response.data[0].id;
                     });
+
+                    // Execute function to load the main video
+                    this.getVideoDetail(this.videoData[0].id);
+
+                    // Otherwise, on mount, load the video ID specified (usually on a refresh)
                 } else {
-                    this.getVideoDetail(videoParams);
+                    // Remove the video clicked/refreshed from the next video sidebar
                     this.nextVideoData = response.data.filter((var1) => {
-                        return var1.id !== videoParams;
+                        return var1.id !== videoIdParams;
                     });
+
+                    // Execute function to load the main video
+                    this.getVideoDetail(videoIdParams);
                 }
             })
             .catch((e) => {
                 console.error(e);
             });
 
-    // GET video detail data with passed information from first axios
-    getVideoDetail = (videoId) => {
+    // Get video detail data with passed information from first axios
+
+    getVideoDetail = (videoIdParam) => {
         axios
             .get(
-                `https://project-2-api.herokuapp.com/videos/${videoId}?api_key=${this.apiKey}`
+                `https://project-2-api.herokuapp.com/videos/${videoIdParam}?api_key=${this.apiKey}`
             )
             .then((response) => {
+                // Store main video details data into object variable
                 this.mainHeroDataDetails = response.data;
+
+                // With the main video data and the sidebar video data, set the state
                 this.setState({
                     currentHero: this.mainHeroDataDetails,
                     dataSummary: this.nextVideoData,
@@ -70,6 +88,7 @@ class Home extends React.Component {
                 console.error(e);
             });
     };
+
     // Lifecycle - Mount Initialization
     componentDidMount() {
         this.getVideos();
@@ -77,18 +96,24 @@ class Home extends React.Component {
 
     // Lifecycle - Updater
     componentDidUpdate(prevProps) {
+        //Store the current video ID and previous video ID into variables
         const videoIdParam = this.props.match.params.videoId;
         const prevId = prevProps.match.params.videoId;
+
         if (videoIdParam) {
+            // If the video Ids are different, update the state with the new video
             if (prevId !== videoIdParam) {
                 axios
+                    // Get new video data from API
                     .get(
                         `https://project-2-api.herokuapp.com/videos/${this.props.match.params.videoId}?api_key=${this.apiKey}`
                     )
                     .then((response) => {
+                        // Remove the main video from sidebar data
                         this.nextVideoData = this.videoData.filter((var1) => {
                             return var1.id !== videoIdParam;
                         });
+                        // Update State with new video and sidebar
                         this.setState({
                             currentHero: response.data,
                             dataSummary: this.nextVideoData,
@@ -99,29 +124,34 @@ class Home extends React.Component {
                     });
                 return true;
             }
-        }
-        if (videoIdParam === undefined && prevId !== videoIdParam) {
-            const videoId = this.videoData[0].id;
-            axios
-                .get(
-                    `https://project-2-api.herokuapp.com/videos/${videoId}?api_key=${this.apiKey}`
-                )
-                .then((response) => {
-                    this.nextVideoData = this.videoData.filter((var1) => {
-                        return var1.id !== videoId;
+
+            // If the code above does not execute, if the ID is undefined(from logo), and if the 2 Ids are different, load the first video from the API
+            if (videoIdParam === undefined && prevId !== videoIdParam) {
+                // Store first video into Id
+                const videoId = this.videoData[0].id;
+                axios
+                    .get(
+                        `https://project-2-api.herokuapp.com/videos/${videoId}?api_key=${this.apiKey}`
+                    )
+                    .then((response) => {
+                        // Remove the main video from sidebar data
+                        this.nextVideoData = this.videoData.filter((var1) => {
+                            return var1.id !== videoId;
+                        });
+                        // Set State
+                        this.setState({
+                            currentHero: response.data,
+                            dataSummary: this.nextVideoData,
+                        });
+                    })
+                    .catch((e) => {
+                        console.error(e);
                     });
-                    this.setState({
-                        currentHero: response.data,
-                        dataSummary: this.nextVideoData,
-                    });
-                })
-                .catch((e) => {
-                    console.error(e);
-                });
-            return true;
+                return true;
+            }
+            // Debugger for infinite loops - comment out during submission
+            // return console.log("Done - Check for infinite loop");
         }
-        // Debugger for infinite loops - comment out during submission
-        return console.log("Done - Check for infinite loop");
     }
 
     render() {
@@ -139,5 +169,3 @@ class Home extends React.Component {
         );
     }
 }
-
-export default Home;
